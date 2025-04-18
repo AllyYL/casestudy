615979ce5257678b0a62d0d3441f6a3b60913def
AllyYL <88838342+AllyYL@users.noreply.github.com>
2025-04-18 19:23:20 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,14 @@
 module.exports = function(eleventyConfig) {
+  // Add a custom filter to get the current year
+  eleventyConfig.addFilter("getYear", function() {
+    return new Date().getFullYear();
+  });
+
+  // Layout aliases
   eleventyConfig.addLayoutAlias("base", "base.njk");
   eleventyConfig.addLayoutAlias("project", "project.njk");
 
+  // Copy static assets
   eleventyConfig.addPassthroughCopy("src/assets");
 
   return {
