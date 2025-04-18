55367436133ac2d897c9060b3420fb3bcdbe9621
Yanxiu Luo <luoyanxiu@gmail.com>
2025-04-18 17:19:49 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,13 @@
 module.exports = function(eleventyConfig) {
-  // Set layout aliases
   eleventyConfig.addLayoutAlias("base", "base.njk");
   eleventyConfig.addLayoutAlias("project", "project.njk");
 
-  // Copy assets directory to _site
   eleventyConfig.addPassthroughCopy("src/assets");
 
   return {
     dir: {
       input: "src",
-      output: "_site",
+      output: "_site"
     }
   };
 };
